9e69aa130258731a81426257044ccc830178347f
ericstotle93 <elu1993@gmail.com>
2018-01-19 00:09:23 -0500

    Finished redux homework

---
+++ src/components/App.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import Cart from '../containers/Cart'
+
+class App extends Component {
+  render() {
+    return (
+      <Cart />
+    )
+  }
+}
+
+export default App;
